d9ba08b58524fc890684ecae5d79866f3240c85b
monte <montenegrojds@gmail.com>
2026-05-03 11:48:52 -0500

    add help and exit REPL commands

--- main.go
+++ main.go
@@ -3,19 +3,51 @@ import
 	("fmt"
 	"bufio"
 	"os"
+	"strings"
 )
+
+type cliCommand struct{
+	name	    string
+	description string
+	callback	func() error
+}
+
+func getCommands() map[string]cliCommand{
+		commands := map[string]cliCommand{
+		"exit":{
+			name:	"exit",
+			description: "Exit the Pokedex",
+			callback: commandExit,
+			},
+			"help":{
+			name: 	"help",
+			description: "Display a help message",
+			callback:	commandHelp, 
+		},
+	}
+	return commands
+}
 
 func main(){
 
-fmt.Println("wellcome to PokedexCLI")
+fmt.Println("Welcome to PokedexCLI")
 scanner := bufio.NewScanner(os.Stdin)
-
+commands :=  getCommands()
 
 for {
 	fmt.Print("Pokedex > ")	
 	if !scanner.Scan(){
 		break
 	}
+
 	input := scanner.Text()
-	fmt.Println(input)
-}}
+	words := strings.Split(input," ")
+	command := strings.ToLower(words[0])
+	cmd, bol := commands[command]
+	if bol{
+		cmd.callback()
+	}else{
+		fmt.Println("Unknow command")
+	}
+	}
+}
